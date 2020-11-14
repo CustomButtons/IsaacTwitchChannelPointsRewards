656085a7adc89ec229209d2f69f67bf900f19dae
CustomButtons <CustomButtons@users.noreply.github.com>
2020-11-15 00:07:34 +0500

    Removed timeout for command

--- mod-client.js
+++ mod-client.js
@@ -5,12 +5,10 @@ client.setEncoding('utf8');
 
 client.connect(8666, '127.0.0.1', function() {
 	console.log('TCP connected');
-    setTimeout(() => {
 	console.log('TCP send getItems command');
-        client.write(JSON.stringify({
-            m: 'getItems'
-        }));
-    }, 5000);
+    client.write(JSON.stringify({
+        m: 'getItems'
+    }));
 });
 
 client.on('data', function(data) {
